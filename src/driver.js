5f2a03ef10bce1e9a0934ff7fe595c0ba3ed8b8b
Chris LaRose <cjlarose@gmail.com>
2016-05-28 18:30:17 -0700

    Returning function that returns constant 42

---
+++ src/driver.js
@@ -0,0 +1,7 @@
+import compile from './compile';
+
+//const view = new Uint8Array([0x00, 0x61, 0x73, 0x6d, 0x0b, 0x00, 0x00, 0x00]);
+const view = compile();
+const wasmModule = Wasm.instantiateModule(view, {});
+
+console.log(wasmModule);
