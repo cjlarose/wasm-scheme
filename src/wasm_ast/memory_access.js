d8005b3a9fea8108ee3a6390382e311a3808ac04
Chris LaRose <cjlarose@gmail.com>
2016-06-11 20:30:16 -0700

    Fix encoding of alignment for stores and loads

--- src/wasm_ast/memory_access.js
+++ src/wasm_ast/memory_access.js
@@ -2,9 +2,9 @@ import { encodeUInt32 } from 'leb';
 
 export function i32Load(addressAst, offset = 0, alignment = 2) {
   // Alignment is interpreted to be an exponent of 2
-  // So alignment = 2 means 4-byte aligned
-  // So alignment = 3 means 8-byte aligned
-  const flags = 1 << alignment;
+  // So alignment = 2 means 4-byte aligned,
+  //    alignment = 3 means 8-byte aligned
+  const flags = alignment;
   return new Uint8Array([
     ...addressAst,
     0x2a,
@@ -14,7 +14,7 @@ export function i32Load(addressAst, offset = 0, alignment = 2) {
 }
 
 export function i32Store(addressAst, valueAst, offset = 0, alignment = 2) {
-  const flags = 1 << alignment;
+  const flags = alignment;
   return new Uint8Array([
     ...addressAst,
     ...valueAst,
